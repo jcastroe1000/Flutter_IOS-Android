942acf55a5239ed512bc4c6b1c944d6b08c343ca
jcastroe1000 <jcastroe1000@alumno.ipn.mx>
2022-06-17 11:28:31 -0500

    adding crud method

--- qr_reader/lib/pages/mapas_page.dart
+++ qr_reader/lib/pages/mapas_page.dart
@@ -4,9 +4,20 @@ class MapasPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Mapas Page'),
-      ),
-    );
+        body: ListView.builder(
+            itemCount: 10,
+            itemBuilder: (_, i) => ListTile(
+                  leading: Icon(
+                    Icons.map,
+                    color: Theme.of(context).primaryColor,
+                  ),
+                  title: Text('http://'),
+                  subtitle: Text('ID: 1'),
+                  trailing: Icon(
+                    Icons.keyboard_arrow_right,
+                    color: Colors.grey,
+                  ),
+                  onTap: () => print('abrir algo...'),
+                )));
   }
 }
---
+++ qr_reader/lib/providers/scan_list_provider.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:qr_reader/providers/db_provider.dart';
+
+class ScanListProvider extends ChangeNotifier {
+  List<ScanModel> scans = [];
+  String tipoSeleccionado = 'http';
+
+  nuevoScan(String valor) async {
+    final nuevoScan = new ScanModel(valor: valor);
+    final id = await DBProvider.db.nuevoScan(nuevoScan);
+    //asignar el ID de la base de datos al modelo
+    nuevoScan.id = id;
+
+    if (this.tipoSeleccionado == nuevoScan.tipo) {
+      this.scans.add(nuevoScan);
+      notifyListeners();
+    }
+  }
+
+  cargarScans() async {
+    final scans = await DBProvider.db.getAllScans();
+    this.scans = [...scans];
+    notifyListeners();
+  }
+
+  cargarScansPorTipo(String tipo) async {
+    final scans = await DBProvider.db.getScansPorTipo(tipo);
+    this.scans = [...scans];
+    this.tipoSeleccionado = tipo;
+    notifyListeners();
+  }
+
+  borrarTodos() async {
+    await DBProvider.db.deleteAll();
+    this.scans = [];
+    notifyListeners();
+  }
+
+  borrarScanById(int id) async {
+    await DBProvider.db.deleteScan(id);
+    this.cargarScansPorTipo(this.tipoSeleccionado);
+  }
+}
