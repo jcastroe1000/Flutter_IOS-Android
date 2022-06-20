1325e1e407e88717a5c1335ca9446d4d8f1ec2ba
jcastroe1000 <jcastroe1000@alumno.ipn.mx>
2022-06-20 16:02:43 -0500

    adding method to open link with lauchUR

--- qr_reader/lib/pages/mapa_page.dart
+++ qr_reader/lib/pages/mapa_page.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:qr_reader/providers/db_provider.dart';
 
 class MapaPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final ScanModel scan =
+        ModalRoute.of(context)!.settings.arguments as ScanModel;
     return Scaffold(
+      appBar: AppBar(title: Text('Mapa')),
       body: Center(
-        child: Text('Mapa Page'),
+        child: Text(scan.valor),
       ),
     );
   }
--- qr_reader/lib/widgets/scan_button.dart
+++ qr_reader/lib/widgets/scan_button.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
 import 'package:provider/provider.dart';
 import 'package:qr_reader/providers/scan_list_provider.dart';
+import 'package:qr_reader/utils/utils.dart';
 
 class ScanButton extends StatelessWidget {
   const ScanButton({Key? key}) : super(key: key);
@@ -13,12 +14,16 @@ class ScanButton extends StatelessWidget {
         onPressed: () async {
           // String barcodeScanRes = await FlutterBarcodeScanner.scanBarcode(
           //     '#3D8BEF', 'Cancelar', false, ScanMode.QR);
-          final barcodeScanRes = 'https://enter.astateoftrance.com/';
+          final barcodeScanRes = 'geo:19.475351,-99.048921';
+          if (barcodeScanRes == '-1') {
+            return;
+          }
 
           final scanListProvider =
               Provider.of<ScanListProvider>(context, listen: false);
-          scanListProvider.nuevoScan(barcodeScanRes);
-          scanListProvider.nuevoScan('geo: 15.39,28.69');
+          final nuevoscan = await scanListProvider.nuevoScan(barcodeScanRes);
+          launchURL(context, nuevoscan);
+          // scanListProvider.nuevoScan('geo: 15.39,28.69');
         },
         child: Icon(Icons.filter_center_focus));
   }
