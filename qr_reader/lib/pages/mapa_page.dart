c6a6645caa70ab95e3041045dc9db374bfa6ef16
jcastroe1000 <jcastroe1000@alumno.ipn.mx>
2022-06-20 17:40:17 -0500

    adding api maps google

--- qr_reader/lib/pages/mapa_page.dart
+++ qr_reader/lib/pages/mapa_page.dart
@@ -1,15 +1,33 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:qr_reader/providers/db_provider.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-class MapaPage extends StatelessWidget {
+class MapaPage extends StatefulWidget {
+  @override
+  State<MapaPage> createState() => _MapaPageState();
+}
+
+class _MapaPageState extends State<MapaPage> {
+  Completer<GoogleMapController> _controller = Completer();
+
   @override
   Widget build(BuildContext context) {
+    final CameraPosition puntoInicial = CameraPosition(
+      target: LatLng(37.42796133580664, -122.085749655962),
+      zoom: 14.4746,
+    );
+
     final ScanModel scan =
         ModalRoute.of(context)!.settings.arguments as ScanModel;
     return Scaffold(
       appBar: AppBar(title: Text('Mapa')),
-      body: Center(
-        child: Text(scan.valor),
+      body: GoogleMap(
+        mapType: MapType.hybrid,
+        initialCameraPosition: puntoInicial,
+        onMapCreated: (GoogleMapController controller) {
+          _controller.complete(controller);
+        },
       ),
     );
   }
