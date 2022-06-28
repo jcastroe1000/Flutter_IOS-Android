afe38f121a4c692579a6787a9619e41fd7e48f46
jcastroe1000 <jcastroe1000@alumno.ipn.mx>
2022-06-28 11:41:29 -0500

    desing product_card screen

--- productos_app/lib/screens/home_screen.dart
+++ productos_app/lib/screens/home_screen.dart
@@ -1,13 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:productos_app/widgets/widgets.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('HomeScreen'),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Center(child: Text('Productos')),
+      ),
+      body: ListView.builder(
+          itemCount: 10,
+          itemBuilder: (BuildContext context, int index) => ProductCard()),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {},
       ),
     );
   }
