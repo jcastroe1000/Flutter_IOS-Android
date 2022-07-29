9d00f14d9910a6aaab6523535764a9cef4667dab
jcastroe1000 <jcastroe1000@alumno.ipn.mx>
2022-07-29 11:44:28 -0500

    creating methods to receive push notifications

--- push_notifications/lib/services/push_notifications_service.dart
+++ push_notifications/lib/services/push_notifications_service.dart
@@ -5,9 +5,25 @@ class PushNotificationsService {
   static FirebaseMessaging messaging = FirebaseMessaging.instance;
   static String? token;
 
+  static Future _backgroundHandler(RemoteMessage message) async {
+    print('onBackground Handler ${message.messageId}');
+  }
+
+  static Future _onMessageHandler(RemoteMessage message) async {
+    print('onMessage Handler ${message.messageId}');
+  }
+
+  static Future _onMessageOpenApp(RemoteMessage message) async {
+    print('onMessageOpenApp Handler ${message.messageId}');
+  }
+
   static Future initializeApp() async {
     await Firebase.initializeApp();
     token = await FirebaseMessaging.instance.getToken();
     print(token);
+
+    FirebaseMessaging.onBackgroundMessage(_backgroundHandler);
+    FirebaseMessaging.onMessage.listen(_onMessageHandler);
+    FirebaseMessaging.onMessageOpenedApp.listen(_onMessageOpenApp);
   }
 }
