b0db1d7d530095f8b7c2ac59de2266bd872cc0f5
jcastroe1000 <jcastroe1000@alumno.ipn.mx>
2022-08-03 13:12:43 -0500

    implements bottomNavigatorBar

--- app_noticias/lib/src/pages/tabs_page.dart
+++ app_noticias/lib/src/pages/tabs_page.dart
@@ -4,11 +4,40 @@ class TabsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: PageView(physics: BouncingScrollPhysics(), children: <Widget>[
+      body: _Paginas(),
+      bottomNavigationBar: _Navegation(),
+    );
+  }
+}
+
+class _Navegation extends StatelessWidget {
+  const _Navegation({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      currentIndex: 1,
+      items: [
+        BottomNavigationBarItem(
+          icon: Icon(Icons.person_outline),
+          label: 'Para ti',
+        ),
+        BottomNavigationBarItem(icon: Icon(Icons.public), label: 'Encabezados'),
+      ],
+    );
+  }
+}
+
+class _Paginas extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return PageView(physics: NeverScrollableScrollPhysics(), children: <Widget>[
       Container(
         color: Colors.red,
       ),
       Container(color: Colors.green)
-    ]));
+    ]);
   }
 }
