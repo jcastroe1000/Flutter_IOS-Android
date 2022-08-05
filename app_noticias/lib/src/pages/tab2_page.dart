a01a1f9dd81d8eb5c19eedc7ba0613ea1899f125
jcastroe1000 <jcastroe1000@alumno.ipn.mx>
2022-08-05 10:41:20 -0500

    creatiing list categories

--- app_noticias/lib/src/pages/tab2_page.dart
+++ app_noticias/lib/src/pages/tab2_page.dart
@@ -1,12 +1,41 @@
+import 'package:app_noticias/src/models/category_model.dart';
+import 'package:app_noticias/src/services/new_service.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class Tab2Page extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Hola Mundo'),
-      ),
+        body: Column(
+      children: <Widget>[
+        Expanded(child: _ListaCategorias()),
+      ],
+    ));
+  }
+}
+
+class _ListaCategorias extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final categories = Provider.of<NewsService>(context).categories;
+
+    return ListView.builder(
+      physics: BouncingScrollPhysics(),
+      scrollDirection: Axis.horizontal,
+      itemCount: categories.length,
+      itemBuilder: (BuildContext context, int index) {
+        return Padding(
+          padding: EdgeInsets.all(15),
+          child: Column(
+            children: [
+              Icon(categories[index].icon),
+              SizedBox(height: 5),
+              Text(categories[index].name)
+            ],
+          ),
+        );
+      },
     );
   }
 }
--- app_noticias/lib/src/services/new_service.dart
+++ app_noticias/lib/src/services/new_service.dart
@@ -1,5 +1,7 @@
+import 'package:app_noticias/src/models/category_model.dart';
 import 'package:app_noticias/src/models/news_models.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:http/http.dart' as http;
 
 final _URL_NEWS = 'https://newsapi.org/v2';
@@ -7,6 +9,15 @@ final _APIKEY = 'ed94b02917ae4b67a1f2aef31855cafd';
 
 class NewsService with ChangeNotifier {
   List<Article> headlines = [];
+  List<Category> categories = [
+    Category(FontAwesomeIcons.building, 'business'),
+    Category(FontAwesomeIcons.tv, 'entertainment'),
+    Category(FontAwesomeIcons.book, 'general'),
+    Category(FontAwesomeIcons.heart, 'health'),
+    Category(FontAwesomeIcons.atom, 'science'),
+    Category(FontAwesomeIcons.baseball, 'sports'),
+    Category(FontAwesomeIcons.laptop, 'technology'),
+  ];
 
   NewsService() {
     this.getTopHeadlines();
